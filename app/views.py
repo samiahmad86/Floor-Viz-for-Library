664cd8b6df170a34b6cda5a36bfe4ea53f4c3322
ZainAhmed <zain.ahmed.sohail@gmail.com>
2017-08-02 05:57:50 +0200

    changed all

--- app/views.py
+++ app/views.py
@@ -5,11 +5,68 @@ import json
 import os
 from pprint import pprint
 import sqlite3
+from collections import defaultdict
+import re
 
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
+	# making search datalist
+	book_info={}
+	total_copies=0
+	filename = os.path.join(app.static_folder, 'books_data_i10.json')
+	with open(filename) as book_data:
+		data = json.load(book_data)
+		for book in data['Books']:
+			if(book['title'] is not "" and book['genres'] is not ""):
+				Hci_WL=['HCI','usability','Interface','Human-Computer Interaction']
+				Hci_WL=re.compile("|".join(Hci_WL))
+
+				SoftSkills_WL=['SoftSkills','Soft Skills']
+				SoftSkills_WL=re.compile("|".join(SoftSkills_WL))
+
+				Ee_WL=['Electr','Schaltung','Elektr','Arduino','EE']
+				Ee_WL=re.compile("|".join(Ee_WL))
+
+				Cs_WL=['Computer Science','CS']
+				Cs_WL=re.compile("|".join(Cs_WL))
+
+				Apple_WL=['Apple','OS X','Mac','Cocoa']
+				Apple_WL=re.compile("|".join(Apple_WL))
+
+				Math_WL=['Math','Statistic']
+				Math_WL=re.compile("|".join(Math_WL))
+
+				Ftd_WL=['Demenz','FTD']
+				Ftd_WL=re.compile("|".join(Ftd_WL))
+
+				History_WL=['Geschichte','History']
+				History_WL=re.compile("|".join(History_WL))
+
+				if(Hci_WL.search(book['genres'])):
+					category = "HCI"
+				elif(SoftSkills_WL.search(book['genres'])):
+					category = "SoftSkills"
+				elif(Ee_WL.search(book['genres'])):
+					category = "EE"
+				elif(Cs_WL.search(book['genres'])):
+					category = "CS"
+				elif(Apple_WL.search(book['genres'])):
+					category = "Apple"
+				elif(Math_WL.search(book['genres'])):
+					category = "Math"
+				elif(History_WL.search(book['genres'])):
+					category = "History"
+				elif(Ftd_WL.search(book['genres'])):
+					category = "FTD"
+	          
+				if(book['title'] in book_info):
+					book_info[book['title']][2]=book_info[book['title']][2]+1
+					total_copies=total_copies+1
+				else:
+					total_copies=total_copies+1
+					book_info[book['title']]=[category,book['shelves'],1]
 	form=book_form()
 	data = models.Books_Data.query.all()
 	ar=[]
@@ -21,20 +78,15 @@ def index():
 		
 	if(form.submit.data):
 			book_found=False
-
-			col=models.Books_Data.query.filter_by(Title=str(form.name.data)).first()
-
-			if(col is not None):
+			str(form.name.data)
+			if(str(form.name.data) in book_info):
 				book_found=True
 				searched=True
-				name=str(col.Title)
-				category=str(col.Category)
-				print(category)
-			if(col is  None):
-				flash("Please enter a book name")
-			elif(book_found==False):
-				flash("Book name "+str(form.name.data)+" does not exist in the Library")
-
+				name=str(form.submit.data)
+				category=str(book_info[str(form.name.data)][0])
+			else:
+				flash("Please enter a book name from the drop down only")
+			
 	return render_template('index.html',form=form,book_data=ar,searched=searched,book_name=name,book_category=category)
 
 @app.route('/shelf', methods=['GET', 'POST'])
